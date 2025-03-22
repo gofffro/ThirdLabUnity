3885df643baeb0700f83b0ad1ec17dc3dbbfe7cc
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-03-22 09:03:33 +0700

    Добавлены флаги, чтобы в консоль методы выводились только один раз

--- Assets/script.cs
+++ Assets/script.cs
@@ -2,43 +2,84 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+  bool isAwakeCalled = false;
+  bool isOnEnableCalled = false;
+  bool isStartCalled = false;
+  bool isUpdateCalled = false;
+  bool isLateUpdateCalled = false;
+  bool isFixedUpdateCalled = false;
+  bool isOnDisableCalled = false;
+  bool isOnDestroyCalled = false;
+
   void Awake()
   {
-    Debug.Log("Awake");
+    if (!isAwakeCalled)
+    {
+      Debug.Log("Awake");
+      isAwakeCalled = true;
+    }
   }
 
   void OnEnable()
   {
-    Debug.Log("OnEnable");
+    if (!isOnEnableCalled)
+    {
+      Debug.Log("OnEnable");
+      isOnEnableCalled = true;
+    }
   }
 
   void Start()
   {
-    Debug.Log("Start");
+    if (!isStartCalled)
+    {
+      Debug.Log("Start");
+      isStartCalled = true;
+    }
   }
 
   void Update()
   {
-    Debug.Log("Update");
+    if (!isUpdateCalled)
+    {
+      Debug.Log("Update");
+      isUpdateCalled = true;
+    }
   }
 
   void LateUpdate()
   {
-    Debug.Log("LateUpdate");
+    if (!isLateUpdateCalled)
+    {
+      Debug.Log("LateUpdate");
+      isLateUpdateCalled = true;
+    }
   }
 
   void FixedUpdate()
   {
-    Debug.Log("FixedUpdate");
+    if (!isFixedUpdateCalled)
+    {
+      Debug.Log("FixedUpdate");
+      isFixedUpdateCalled = true;
+    }
   }
 
   void OnDisable()
   {
-    Debug.Log("OnDisable");
+    if (!isOnDisableCalled)
+    {
+      Debug.Log("OnDisable");
+      isOnDisableCalled = true;
+    }
   }
 
   void OnDestroy()
   {
-    Debug.Log("OnDestroy");
+    if (!isOnDestroyCalled)
+    {
+      Debug.Log("OnDestroy");
+      isOnDestroyCalled = true;
+    }
   }
 }
